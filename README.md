4d5bc6f51fd0a909ecd77a96fe704110269beed2
teralove <terascripts@gmail.com>
2018-03-24 06:24:15 -0400

    1.4 update

--- README.md
+++ README.md
@@ -1,28 +1,37 @@
-# Party Death Markers
-Spawns large visible markers on top of dead party members.
+# party-death-markers
+tera-proxy module that spawns large beacons on top of dead party members.
 
-## Chat commands:
-* **!PartyDeathMarkers**    - Toggles the module off/on
-* **!ClearDeathMarkers**    - Removes all present markers
+## Dependency
+- `Command` module
 
-Commands are not case-sensitive. [slash](https://github.com/baldera-mods/slash) is supported but not required
+## Usage
+### `PartyDeathMarkers`
+- Toggle on/off
+- Default is on
 
-## Known issues:
-* If you logout, any present markers will be removed.
-* When someone dies, you have to be present for the markers to spawn.
+## Info
+- Tanks = red. Healers = blue. DPS = tall beacon.
+- The item cannot be picked up. It's fake. Spawned client-side for your eyes only. No body else can see them.
 
-## Info:
-* Tanks = red relic piece. Healers = blue relic piece. DPS = tall rare item beacon.
-* No you cannot pickup the item, it's fake. Spawned client-side just for the visual effect and only you can see them.
-* Any item can be spawned, such as relic pieces which have different visual effects. See index.js for list.
+## Changelog
+<details>
 
-## Changelog 
-### 1.2.0
-* [+] Added class specific markers
-### 1.1.1
-* [+] Optimized a couple functions
-### 1.1.0
-* [+] Fixed bug with markers spawning on wrong positions
+    1.40
+    - Updated code aesthetics
+    - Fixed bug with beacons spawning on players no longer in party.
+    - Added S_SPAWN_USER hook. Fixes issues with beacons not spawning.
+    1.31
+    - Removed ClearDeathMarkers command
+    1.30
+    - Added Command dependency
+    - Removed slash support
+    1.20
+    - Added class specific markers
+    1.11
+    - Optimized a couple functions
+    1.10
+    - Fixed bug with markers spawning on wrong positions
+    </details>
 
-
+---
 ![Screenshot](http://i.imgur.com/bOSA6Lx.jpg)
--- index.js
+++ index.js
@@ -1,209 +1,166 @@
-// vers 1.2.0
-
-/*
-Item IDs that have unique glows
-
-91116	Amarun's Relic Piece
-55658	Bahaar's Relic Piece
-91166	Dagon's Relic Piece
-91114	Elinu's Relic Piece
-91118	Gidd's Relic Piece
-91177	Ishara's Relic Piece
-91113	Isren's Relic Piece
-91119	Karas's Relic Piece
-91188	Oriyn's Relic Piece
-57000	Seren's Relic Piece
-91115	Tithus's Relic Piece
-91117	Zuras's Relic Piece
-98260	Vergos's Head			// Vergo pieces all have the same effect
-98263	Vergos's Horn
-98262	Vergos's Scale
-98264	Vergos's Bone
-98261	Vergos's Fang
+/* 
+OPCODE REQUIRED :
+- S_LOGIN
+- S_PARTY_MEMBER_LIST
+- S_DEAD_LOCATION
+- S_SPAWN_USER
+- S_PARTY_MEMBER_STAT_UPDATE
+- S_LEAVE_PARTY_MEMBER
+- S_LEAVE_PARTY
 */
-const ItemToSpawn = 98260;
-const useJobSpecificMarkers = true;
-/*
-warrior = 0, lancer = 1, slayer = 2, berserker = 3,
-sorcerer = 4, archer = 5, priest = 6, mystic = 7,
-reaper = 8, gunner = 9, brawler = 10, ninja = 11,
-valkyrie = 12
-*/
-const jobSpecificMarkers = [	
-	{
-		// tanks
-		jobs: [1, 10], 
-		marker: 91177
-	},
-	{
-		// healers
-		jobs: [6, 7], 
-		marker: 91113
-	},	
-];
-
-const format = require('./format.js');
+const Command = require('command');
 
 module.exports = function PartyDeathMarkers(dispatch) {
-
-	let enabled = true;
-	let cid, userId;
-	let deadPeople = [];	
-	let partyMembers = [];
-			
-	dispatch.hook('S_LOGIN', 1, (event) => {	
-		cid = event.cid;
-		removeAll();
+    const command = Command(dispatch);
+    
+    const DefaultItemSpawn = 98260;
+    const UseJobSpecificMarkers = true;
+    /*
+    warrior = 0, lancer = 1, slayer = 2, berserker = 3,
+    sorcerer = 4, archer = 5, priest = 6, mystic = 7,
+    reaper = 8, gunner = 9, brawler = 10, ninja = 11, valkyrie = 12
+    */
+    const JobSpecificMarkers = [
+        {
+            // tanks
+            jobs: [1, 10], 
+            marker: 91177
+        },
+        {
+            // healers
+            jobs: [6, 7], 
+            marker: 91113
+        }
+    ];
+    
+    let enabled = true;
+    let playerId = 0;
+    let partyMembers = [];
+    let spawnedBeacons = [];
+    
+    dispatch.hook('S_LOGIN', 9, (event) => {
+        playerId = event.playerId;
+        removeAllMarkers();
     })
-	
-	dispatch.hook('S_PARTY_MEMBER_LIST', 4, (event) => {
-		if (!enabled) return;
-		partyMembers = [];
-		for(let i in event.members) {
-			if (cid - event.members[i].cID === 0) {
-				userId = event.members[i].playerId
-			} else {
-				partyMembers.push({
-					cid: event.members[i].cID,
-					playerId: event.members[i].playerId,
-					job: event.members[i].class,
-					x: 0,
-					y: 0,
-					z: 0
-				});
-			}
-		}
+        
+    dispatch.hook('S_PARTY_MEMBER_LIST', 6, (event) => {
+        partyMembers = event.members;
     })
-		
- 	dispatch.hook('S_DEAD_LOCATION', 1, (event) => {
-		 if (!enabled) return;
-		 for(let i in partyMembers) {
-			 if (partyMembers[i].cid - event.target == 0) {	
-				 partyMembers[i].x = event.x;
-				 partyMembers[i].y = event.y;
-				 partyMembers[i].z = event.z;
-				 spawnMarker(partyMembers[i].playerId);
-				 return;
-			 }
-		}
-	})
-	 		
-	dispatch.hook('S_PARTY_MEMBER_STAT_UPDATE', 2, (event) => {
-		if (!enabled || event.playerId == userId) return;
-		if (event.curHp > 0) {
-			removeMarker(event.playerId);
-		}
-	})
-		 
-	function spawnMarker(playerId) {	
-		if (deadPeople.includes(playerId)) {
-			dispatch.toClient('S_DESPAWN_DROPITEM', 1, {
-				id: playerId
-			});	
-		} else {
-			deadPeople.push(playerId);	
-		}
-			
-		let index = getIndexOfPlayerId(partyMembers, playerId);
-		
-		dispatch.toClient('S_SPAWN_DROPITEM', 1, {
-			id: playerId,
-			x: partyMembers[index].x,
-			y: partyMembers[index].y,
-			z: partyMembers[index].z,
-			item: getSpawnItem(partyMembers[index].job),
-			amount: 1,
-			expiry: 999999,
-			owners: [{id: userId}]
-		});	
-	}
-	
-	function removeMarker(playerId) {
-		if (deadPeople.includes(playerId)) {
-			let index = deadPeople.indexOf(playerId);
-			deadPeople.splice(index, 1);
-			
-			dispatch.toClient('S_DESPAWN_DROPITEM', 1, {
-				id: playerId
-			});	
-		}
-	}
-	
-	function removeAll() {
-		for (let i in deadPeople) {
-			removeMarker(deadPeople[i]);
-		}
-	}
-	
-	function getIndexOfPlayerId(arr, playerId) {
-		for (var i=0; i<arr.length; i++) {
-			if (arr[i].playerId-playerId==0) return i;
-		}
-		return -1;
-	}
-	
-	
-	function getSpawnItem(jobId) {
-		if (useJobSpecificMarkers) {
-			for(let i in jobSpecificMarkers) {
-				if (jobSpecificMarkers[i].jobs.includes(jobId))
-					return jobSpecificMarkers[i].marker;
-			}		
-		}
-		
-		return ItemToSpawn;		
-	}
-	
-	dispatch.hook('S_LEAVE_PARTY_MEMBER', 2, (event) => {
-		removeMarker(event.playerId);
-	})	
-	
+    
+    dispatch.hook('S_DEAD_LOCATION', 2, (event) => {
+        for (i = 0; i < partyMembers.length; i++) { 
+            if (partyMembers[i].gameId.equals(event.gameId)) {
+                spawnMarker(partyMembers[i].playerId, event.loc);
+                return;
+            }
+        }
+    })
+    
+    dispatch.hook('S_SPAWN_USER', 13, (event) => {
+        if (!event.alive) {
+            for (i = 0; i < partyMembers.length; i++) { 
+                if (partyMembers[i].gameId.equals(event.gameId)) {
+                    spawnMarker(partyMembers[i].playerId, event.loc);
+                    return;
+                }
+            }
+        }
+    })
+    
+    dispatch.hook('S_PARTY_MEMBER_STAT_UPDATE', 2, (event) => {
+        if (playerId == event.playerId) return;
+        
+        if (event.curHp > 0 || !event.online) {
+            for (i = 0; i < partyMembers.length; i++) { 
+                if (partyMembers[i].playerId == event.playerId) {
+                    removeMarker(event.playerId);
+                    return;
+                }
+            }
+        }
+    })
+    
+        
+    dispatch.hook('S_LEAVE_PARTY_MEMBER', 2, (event) => {
+        for (i = 0; i < partyMembers.length; i++) {
+            if (partyMembers[i].playerId == event.playerId) {
+                removeMarker(partyMembers[i].gameId);
+            }
+        }
+    })
+    
     dispatch.hook('S_LEAVE_PARTY', 1, (event) => {
-		removeAll();
-	})	
-	
-    const chatHook = event => {		
-		let command = format.stripTags(event.message).split(' ');
-		
-		if (['!cleardeathmarkers'].includes(command[0].toLowerCase())) {
-			removeAll();
-			return false;
-		} else if (['!partydeathmarkers'].includes(command[0].toLowerCase())) {
-			toggleModule();
-			return false;
-		}
-	}
-	dispatch.hook('C_CHAT', 1, chatHook)	
-	dispatch.hook('C_WHISPER', 1, chatHook)
-  
-	function toggleModule() {
-		enabled = !enabled;
-		if (!enabled) removeAll();
-		systemMsg( enabled ? 'enabled' : 'disabled' );
-	}
-	
-	// slash support
-	try {
-		const Slash = require('slash')
-		const slash = new Slash(dispatch)
-		slash.on('cleardeathmarkers', args => removeAll())
-		slash.on('partydeathmarkers', args => toggleModule())
-	} catch (e) {
-		// do nothing because slash is optional
-	}
-	
-	
-	function systemMsg(msg) {
-		dispatch.toClient('S_CHAT', 1, {
-			channel: 24,
-			authorID: 0,
-			unk1: 0,
-			gm: 0,
-			unk2: 0,
-			authorName: '',
-		message: ' (party-death-markers) ' + msg
-		});
-	}
-	
-	
+        removeAllMarkers();
+        partyMembers = [];
+    })
+    
+    function spawnMarker(id, loc) {
+        if (!enabled) return;
+        if (playerId == id) return;
+        
+        if (spawnedBeacons.includes(id)) {
+            dispatch.toClient('S_DESPAWN_DROPITEM', 4, {
+                gameId: id
+            });
+        } else {
+            spawnedBeacons.push(id);
+        }
+        
+        dispatch.toClient('S_SPAWN_DROPITEM', 6, {
+            gameId: id,
+            loc: loc,
+            item: getSpawnItem(id),
+            amount: 1,
+            expiry: 999999,
+            owners: [{playerId: playerId}]
+        });	
+    }
+    
+    function removeMarker(id) {
+        if (spawnedBeacons.includes(id)) {
+            let index = spawnedBeacons.indexOf(id);
+            spawnedBeacons.splice(index, 1);
+            
+            dispatch.toClient('S_DESPAWN_DROPITEM', 4, {
+                gameId: id
+            });
+        }
+    }
+    
+    function removeAllMarkers() {
+        for (i = 0; i < spawnedBeacons.length; i++) { 
+            removeMarker(spawnedBeacons[i]);
+        }
+        spawnedBeacons = [];
+    }
+    
+    function getSpawnItem(id) {
+        if (UseJobSpecificMarkers) {
+            let jobId;
+            for (i = 0; i < partyMembers.length; i++) { 
+                if (partyMembers[i].playerId == id) {
+                    jobId = partyMembers[i].class;
+                }
+            }
+            
+            for (i = 0; i < JobSpecificMarkers.length; i++) { 
+                if (JobSpecificMarkers[i].jobs.includes(jobId)) {
+                    return JobSpecificMarkers[i].marker;
+                }
+            }
+        }
+        
+        return DefaultItemSpawn;
+    }
+    
+    function toggleModule() {
+        enabled = !enabled;
+        if (!enabled) removeAllMarkers();
+        command.message('(party-death-markers) ' + (enabled ? 'enabled' : 'disabled'));
+    }
+    
+    command.add('partydeathmarkers', () => {
+        toggleModule()
+    });
+    
 }
