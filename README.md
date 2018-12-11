44055df5a040e351698f54fd1d8026727081135f
teralove <terascripts@gmail.com>
2018-12-10 20:43:01 -0500

    Next Gen update

--- README.md
+++ README.md
@@ -1,37 +1,9 @@
 # party-death-markers
-tera-proxy module that spawns large beacons on top of dead party members.
-
-## Usage
-### `PartyDeathMarkers`
-- Toggle on/off
-- Default is on
+Spawns large beacons on top of dead party members.
 
 ## Info
 - Tanks = red. Healers = blue. DPS = tall beacon.
 - The item cannot be picked up. It's fake. Spawned client-side for your eyes only. No body else can see them.
-
-## Changelog
-<details>
-
-    1.32
-    - Fixed bug: Beacons despawning and sometimes not spawning.
-    1.31
-    - Supports Caali's proxy auto-updater
-    1.30
-    - Updated code aesthetics
-    - Fixed bug with beacons spawning on players no longer in party.
-    - Added S_SPAWN_USER hook. Fixes issues with beacons not spawning.
-    - Removed ClearDeathMarkers command
-    - Added Command dependency
-    - Removed slash support
-    1.20
-    - Added class specific markers
-    1.11
-    - Optimized a couple functions
-    1.10
-    - Fixed bug with markers spawning on wrong positions
-
-</details>
 
 ---
 
--- index.js
+++ index.js
@@ -1,146 +1,84 @@
-const Command = require('command');
+module.exports = function PartyDeathMarkers(mod) {
+  let members = []
+  let markers = []
 
-module.exports = function PartyDeathMarkers(dispatch) {
-    const command = Command(dispatch);
-    
-    const DefaultItemSpawn = 98260;
-    const UseJobSpecificMarkers = true;
-    /*
-    warrior = 0, lancer = 1, slayer = 2, berserker = 3,
-    sorcerer = 4, archer = 5, priest = 6, mystic = 7,
-    reaper = 8, gunner = 9, brawler = 10, ninja = 11, valkyrie = 12
-    */
-    const JobSpecificMarkers = [
-        {
-            // tanks
-            jobs: [1, 10], 
-            marker: 91177
-        },
-        {
-            // healers
-            jobs: [6, 7], 
-            marker: 91113
-        }
-    ];
-    
-    let enabled = true;
-    let playerId = 0;
-    let partyMembers = [];
-    let spawnedBeacons = [];
-    
-    dispatch.hook('S_LOGIN', 10, (event) => {
-        playerId = event.playerId;
-        removeAllMarkers();
+  mod.game.on('enter_game', removeAllMarkers)
+
+  mod.hook('S_PARTY_MEMBER_LIST', 7, event => {
+    members = event.members
+  })
+
+  mod.hook('S_DEAD_LOCATION', 2, event => {
+    spawnMarker(members.find(member => member.gameId === event.gameId), event.loc)
+  })
+
+  mod.hook('S_SPAWN_USER', 13, event => {
+    if (!event.alive)
+      spawnMarker(members.find(member => member.gameId === event.gameId), event.loc)
+  })
+
+  mod.hook('S_PARTY_MEMBER_STAT_UPDATE', 3, event => {
+    if (mod.game.me.playerId === event.playerId)
+      return
+
+    if (event.curHp > 0)
+      removeMarker(members.find(member => member.playerId === event.playerId))
+  })
+
+  mod.hook('S_LEAVE_PARTY_MEMBER', 2, event => {
+    removeMarker(members.find(member => member.playerId === event.playerId))
+  })
+
+  mod.hook('S_LEAVE_PARTY', 1, () => {
+    removeAllMarkers()
+    members = []
+  })
+
+  function spawnMarker(member, loc) {
+    if (!member || mod.game.me.is(member.gameId))
+      return
+
+    removeMarker(member)
+    markers.push(member.playerId)
+
+    mod.toClient('S_SPAWN_DROPITEM', 6, {
+      gameId: member.playerId,
+      loc: loc,
+      item: getMarker(member.class),
+      amount: 1,
+      expiry: 999999,
+      owners: [{playerId: mod.game.me.playerId}]
     })
-        
-    dispatch.hook('S_PARTY_MEMBER_LIST', 7, (event) => {
-        partyMembers = event.members;
-    })
-    
-    dispatch.hook('S_DEAD_LOCATION', 2, (event) => {
-        for (let i = 0; i < partyMembers.length; i++) { 
-            if (partyMembers[i].gameId.equals(event.gameId)) {
-                spawnMarker(partyMembers[i].playerId, event.loc);
-                return;
-            }
-        }
-    })
-    
-    dispatch.hook('S_SPAWN_USER', 13, (event) => {
-        if (!event.alive) {
-            for (let i = 0; i < partyMembers.length; i++) { 
-                if (partyMembers[i].gameId.equals(event.gameId)) {
-                    spawnMarker(partyMembers[i].playerId, event.loc);
-                    return;
-                }
-            }
-        }
-    })
-    
-    dispatch.hook('S_PARTY_MEMBER_STAT_UPDATE', 3, (event) => {
-        if (playerId == event.playerId) return;
-        
-        if (event.curHp > 0) {
-            for (let i = 0; i < partyMembers.length; i++) { 
-                if (partyMembers[i].playerId == event.playerId) {
-                    removeMarker(event.playerId);
-                    return;
-                }
-            }
-        }
-    })
-    
-    dispatch.hook('S_LEAVE_PARTY_MEMBER', 2, (event) => {
-        for (let i = 0; i < partyMembers.length; i++) {
-            if (partyMembers[i].playerId == event.playerId) {
-                removeMarker(partyMembers[i].gameId);
-            }
-        }
-    })
-    
-    dispatch.hook('S_LEAVE_PARTY', 1, (event) => {
-        removeAllMarkers();
-        partyMembers = [];
-    })
-    
-    function spawnMarker(id, loc) {
-        if (!enabled) return;
-        if (playerId == id) return;
-        
-        removeMarker(id); //refresh
-        spawnedBeacons.push(id);
-        
-        dispatch.toClient('S_SPAWN_DROPITEM', 6, {
-            gameId: id,
-            loc: loc,
-            item: getSpawnItem(id),
-            amount: 1,
-            expiry: 999999,
-            owners: [{playerId: playerId}]
-        });
+  }
+
+  function getMarker(classid) {
+    switch (classid) {
+      case 1:
+      case 10:
+        return 91177
+      case 6:
+      case 7:
+        return 91113
+      default:
+        return 98260
     }
-    
-    function removeMarker(id) {
-        if (spawnedBeacons.includes(id)) {
-            let index = spawnedBeacons.indexOf(id);
-            spawnedBeacons.splice(index, 1);
-            
-            dispatch.toClient('S_DESPAWN_DROPITEM', 4, {
-                gameId: id
-            });
-        }
+  }
+
+  function removeMarker(member) {
+    if (!member)
+      return
+
+    const id = member.playerId
+    if (markers.includes(id)) {
+      mod.toClient('S_DESPAWN_DROPITEM', 4, {
+        gameId: id
+      })
+      markers = markers.filter(marker => marker !== id)
     }
-    
-    function removeAllMarkers() {
-        for (let i = 0; i < spawnedBeacons.length; i++) { 
-            removeMarker(spawnedBeacons[i]);
-        }
-        spawnedBeacons = [];
-    }
-    
-    function getSpawnItem(id) {
-        if (UseJobSpecificMarkers) {
-            let jobId;
-            for (let i = 0; i < partyMembers.length; i++) { 
-                if (partyMembers[i].playerId == id) {
-                    jobId = partyMembers[i].class;
-                }
-            }
-            
-            for (let i = 0; i < JobSpecificMarkers.length; i++) { 
-                if (JobSpecificMarkers[i].jobs.includes(jobId)) {
-                    return JobSpecificMarkers[i].marker;
-                }
-            }
-        }
-        
-        return DefaultItemSpawn;
-    }
-    
-    command.add('partydeathmarkers', () => {
-        enabled = !enabled;
-        if (!enabled) removeAllMarkers();
-        command.message('(party-death-markers) ' + (enabled ? 'enabled' : 'disabled'));
-    });
-    
+  }
+
+  function removeAllMarkers() {
+    members.forEach(member => removeMarker(member))
+    markers = []
+  }
 }
